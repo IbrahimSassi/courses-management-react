a354eb461e9ec1a6a7fc85907d3e11b28aa67e5e
IbrahimSassi <ibrahim.sassi@esprit.tn>
2017-02-24 23:16:10 +0100

    author component + author Api

---
+++ src/api/authorData.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Ibrahim on 24/02/2017.
+ */
+module.exports = {
+    authors:
+        [
+            {
+                id: 'cory-house',
+                firstName: 'Cory',
+                lastName: 'House'
+            },
+            {
+                id: 'scott-allen',
+                firstName: 'Scott',
+                lastName: 'Allen'
+            },
+            {
+                id: 'dan-wahlin',
+                firstName: 'Dan',
+                lastName: 'Wahlin'
+            },
+            {
+                id: 'john-papa',
+                firstName: 'John',
+                lastName: 'Papa'
+            }
+        ]
+};
