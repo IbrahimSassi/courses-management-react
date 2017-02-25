e081ce55f0e26a44b8280fec1fc79b59dafcf31d
IbrahimSassi <ibrahim.sassi@esprit.tn>
2017-02-25 23:19:32 +0100

    Location + Changing to Html 5 History style routing

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -2,7 +2,23 @@
 var React = require('react');
 
 var About = React.createClass({
-    render: function(){
+    statics: {
+        willTransitionTo: function (transition, params, query, callback) {
+            if (!confirm('Are you sure you want to read a page thats this boring ')) {
+                transition.abort();
+            }
+            else {
+                callback();
+            }
+        },
+        willTransitionFrom: function (transition, component) {
+            if (!confirm('Are you sure to leave a page thats this exiting ')) {
+                transition.abort();
+            }
+        }
+    },
+
+    render: function () {
         return (
             <div>
                 <h1>About</h1>
