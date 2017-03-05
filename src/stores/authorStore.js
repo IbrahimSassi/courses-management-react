4fb24a147ece564ccaa738723364e16108e96c29
IbrahimSassi <ibrahim.sassi@esprit.tn>
2017-03-05 15:27:09 +0100

    Create our first store +Change Listener

---
+++ src/stores/authorStore.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Ibrahim on 05/03/2017.
+ */
+"use strict";
+
+var Dispatcher = require('../dispatcher/appDispatcher');
+var ActionsTypes = require('../constants/actionTypes');
+var EventEmitter = require('events').EventEmitter;
+var assign = require('object-assign');
+var CHANGE_EVENT = "change";
+
+
+var authorStore = assign({}, EventEmitter.prototype, {
+    addChangeListener: function (callback) {
+        this.on(CHANGE_EVENT, callback);
+    },
+    removeChangeListener: function (callback) {
+        this.removeListener(CHANGE_EVENT, callback);
+    },
+    emitChange:function () {
+        this.emit(CHANGE_EVENT);
+    }
+})
